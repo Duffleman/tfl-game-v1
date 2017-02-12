7caf58f34fb588431fe51ba9a250558b9f0b21a8
George Miller <git@duffleman.co.uk>
2017-02-12 23:17:52 +0000

    Birth of the UI

--- app/Http/Requests/NewGameStateRequest.php
+++ app/Http/Requests/NewGameStateRequest.php
@@ -24,7 +24,7 @@ class NewGameStateRequest extends FormRequest
     public function rules()
     {
         return [
-            'player' => 'string',
+            'player' => '',
         ];
     }
 }
