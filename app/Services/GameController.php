2c4c226831e3dda567ec14019aa3565417af6ec0
George Miller <git@duffleman.co.uk>
2017-02-19 19:12:46 +0000

    Clean up & Fixes #1

--- app/Services/GameController.php
+++ app/Services/GameController.php
@@ -7,7 +7,8 @@ use TFLGame\GameState;
 
 class GameController
 {
-    public static function getName(GameState $state, $answered) {
+    public static function getName(GameState $state, $answered)
+    {
         $alreadyGiven = $answered->map(function ($ans) {
             return $ans->answer;
         });
@@ -15,14 +16,14 @@ class GameController
         $lines = $state->lines()->get()->pluck('id')->toArray();
 
         $station = Station::whereNotIn('cleanName', $alreadyGiven)
-            ->whereHas('lines', function($query) use($lines) {
+            ->whereHas('lines', function ($query) use ($lines) {
                 $query->whereIn('line_id', $lines);
             })
             ->get()
             ->random();
 
         $space = ' ';
-        $text = preg_replace("/[AEIOU12345]/", "", $station->cleanName);
+        $text = preg_replace("/[AEIOU]/", "", $station->cleanName);
         $text = str_replace("_", "", $text);
 
         $spaceCount = spaceCount(strlen($text));
@@ -45,18 +46,21 @@ class GameController
     }
 }
 
-function getPosition($array, &$used = []) {
+function getPosition($array, &$used = [])
+{
     $position = rand(1, count($array) - 1);
 
-    if ($array[$position] === ' ')
+    if ($array[$position] === ' ') {
         return getPosition($array, $used);
-    else
+    } else {
         $used[] = $position;
+    }
 
     return $position;
 }
 
-function spaceCount($wordLength) {
+function spaceCount($wordLength)
+{
     $max = 4;
     $count = round($wordLength / 2, 0, PHP_ROUND_HALF_DOWN);
 
